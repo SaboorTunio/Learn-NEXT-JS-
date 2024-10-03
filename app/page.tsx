159c70dc832eb4878e7de30d8388aae6b590638a
[Saboor Tunio] <[saboortunio2021@gmail.com]>
2024-10-03 19:30:29 +0500

    My First Hello World! Project In NEXT.JS

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-blue-900 m-auto size-96 pl-24 pr-20 text-4xl pt-24 text-blue-500"><h1>Hello User</h1>
+    <h2>Wel Come</h2>
+    <h3>To NEXT.JS</h3>
+    </div>
+  );
+}
